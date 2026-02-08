3e982a072543183bedc415f9ec03875d8273fb82
erdemozkn <erdemzkn7@gmail.com>
2026-02-09 02:12:42 +0300

    simulation fixes

--- denis_v1_bringup/launch/launch_sim.launch.py
+++ denis_v1_bringup/launch/launch_sim.launch.py
@@ -1,11 +1,11 @@
 import os
 
-from ament_index_python import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory
 
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
@@ -14,15 +14,36 @@ def generate_launch_description():
     pkg_denis_bringup = get_package_share_directory('denis_v1_bringup')
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    pkg_denis_bringup, 'launch', 'rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(pkg_denis_bringup, 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'use_ros2_control': 'true'
+        }.items()
     )
+
+    joystick = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            pkg_denis_bringup, 'launch', 'joystick.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(pkg_denis_bringup, 'config', 'twist_mux.yaml')
+    twist_mux = Node(
+        package="twist_mux",
+        executable="twist_mux",
+    parameters=[
+        twist_mux_params,
+        {'use_stamped': False}
+    ],
+    remappings=[('/cmd_vel_out', '/diff_drive_controller/cmd_vel_unstamped')]
+)
 
     default_world = os.path.join(pkg_denis_bringup, 'worlds', 'nav2_test.sdf')
 
     world = LaunchConfiguration('world')
-    
+
     world_arg = DeclareLaunchArgument(
         'world',
         default_value=default_world,
@@ -30,49 +51,67 @@ def generate_launch_description():
     )
 
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
             )
-    
-    spawn_entity = Node(package='ros_gz_sim', 
-                        executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'denis',
-                                   '-x', '-1.0',
-                                   '-y', '1.0',
-                                   '-z', '0.1'],
-                        output='screen')
-    
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_cont"],
+        ]),
+        launch_arguments={
+            'gz_args': LaunchConfiguration('gz_args', default=['-r -v 4 ', world]),
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'denis',
+            '-x', '-1.0',
+            '-y', '1.0',
+            '-z', '0.1'
+        ],
+        output='screen'
     )
 
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_broad"],
+        arguments=["joint_state_broadcaster", "--controller-manager-timeout", "35.0"],
+        output='screen'
     )
 
-    bridge_params = os.path.join(pkg_denis_bringup,'config','gazebo_bridge.yaml')
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "--controller-manager-timeout", "35.0"],
+        output='screen'
+    )
+
+    bridge_params = os.path.join(pkg_denis_bringup, 'config', 'gazebo_bridge.yaml')
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
-        arguments=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ]
+        arguments=['--ros-args', '-p', f'config_file:={bridge_params}'],
+        output='screen'
     )
 
     return LaunchDescription([
         rsp,
+        joystick,
+        twist_mux,
         world_arg,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner,
         ros_gz_bridge,
+        TimerAction(
+            period=6.0,
+            actions=[
+                joint_broad_spawner,
+                diff_drive_spawner
+            ]
+        ),
     ])
